c84beb5ef9895cdfb7363d5a71419de151de97c0
Ярослав Дубницький <ipz214_dyav@student.ztu.edu.ua>
2024-07-18 11:39:14 +0300

    Creat Pages

--- spstor/myProject/views.py
+++ spstor/myProject/views.py
@@ -5,3 +5,18 @@ def index(request):
 
 def about(request):
     return render(request, 'myProject/about.html')
+
+def login(request):
+    return render(request, 'myProject/login.html')
+
+def register(request):
+    return render(request, 'myProject/register.html')
+
+def library(request):
+    return render(request, 'myProject/library.html')
+
+def games(request):
+    return render(request, 'myProject/games.html')
+
+def categories(request):
+    return render(request, 'myProject/categories.html')
